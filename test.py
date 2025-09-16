508bd18cbed43c9ed3fec1efc47e8961166b71e0
Isaac <isaacngrwi@gmail.com>
2025-09-16 22:18:23 +0100

    Added d1, d2 calculations for the black scholes pricing model

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
+import math
 import scipy
 
-underlying_price = 42  # price of stock rn
-strike_price = 40  # right to buy the stock at $
-time_to_expire = 0.5  # when the option expires (years)
-risk_free_rate = 0.04  # risk free returns (percentage as a number) ex: uk treasury bill returns
-volatility = 0.2  # standard deviation for stock (percentage as a number)
+S = 42  # underlying price ie price of stock rn
+K = 40  # strike price ie right to buy the stock at $
+T = 0.5  # time to expiration ie when the option expires (years)
+r = 0.04  # risk-free returns (percentage as a number) ex: uk treasury bill returns
+vol = 0.2  # volatility ie standard deviation for stock (percentage as a number)
+
+# black scholes calculations
+d1 = (math.log(S/K) + (r + vol**2 * 0.5) * T) / (vol * math.sqrt(T))
+print(d1)
+d2 = (d1 - (vol * math.sqrt(T)))
+print(d2)
+
