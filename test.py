9631772ecccedf3917469756ce2ed81ab83890ab
Isaac <isaacngrwi@gmail.com>
2025-09-16 22:11:53 +0100

    Added test.py for testing functions

---
+++ test.py
@@ -0,0 +1,7 @@
+import scipy
+
+underlying_price = 42  # price of stock rn
+strike_price = 40  # right to buy the stock at $
+time_to_expire = 0.5  # when the option expires (years)
+risk_free_rate = 0.04  # risk free returns (percentage as a number) ex: uk treasury bill returns
+volatility = 0.2  # standard deviation for stock (percentage as a number)
