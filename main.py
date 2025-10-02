d6eb15acc43bd7928e5096ea06057323bd7da9c0
IsaacRWI <isaacngrwi@gmail.com>
2025-10-02 17:26:46 +0100

    Added basic CLI for calling function

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-import math
-from scipy.stats import norm
+from blackScholesCalc import BlackScholesCalc
+
+while True:
+    selection = input("""Please select options pricing model:
+    1. Black Scholes Model
+    Selection: """)
+    match selection:
+        case "1":
+            print("Black Scholes")
+        case _:
+            print("no matches")
