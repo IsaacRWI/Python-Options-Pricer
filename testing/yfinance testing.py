3520080db7b6c84e976969aff41d1e14cb70006a
IsaacRWI <isaacngrwi@gmail.com>
2025-10-02 17:00:41 +0100

    Added testing for yfinance - Added yfinance to requirements.txt - Tested retrieving live stock price from yahoo finance

--- testing/yfinance testing.py
+++ testing/yfinance testing.py
@@ -1,5 +1,21 @@
-from dotenv import load_dotenv
-import os
+import yfinance as yf
+from datetime import datetime
 
-load_dotenv()
-token = os.getenv("AV_APIKEY")
+def dateToString(date):
+    date = date.strftime("%Y-%m-%d")
+    return date
+
+# df = yf.download("GOOG", start=dateToString(datetime.now()), multi_level_index=False)
+
+# print(dateToString(datetime.now()))
+# print(df.info)
+
+# print(df[["Close"]])
+
+# close = df["Close"].tolist()[0]
+# print(close)
+
+df = yf.download("GOOG", start=datetime.now().strftime("%Y-%m-%d"), multi_level_index=False, auto_adjust=True)
+# print(df.info)
+close = df["Close"].tolist()[0]
+print(close)
